a9d94a2467f1bbc8fffbd5c37c3db7266d5cc8be
Adrian <modernamun@gmail.com>
2014-05-08 12:02:01 -0400

    Update my_solution.js

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -4,8 +4,93 @@
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
- 
- 
+       // Rescue Mission
+// this.moveDown();
+// this.moveDown();
+// this.moveRight();
+// this.moveUp();
+// this.moveUp();
+// this.moveRight();
+// this.moveRight();
+// this.moveRight();
+// this.moveDown();
+// this.moveDown();
+// this.attackNearbyEnemy()
+
+
+      //Grab the Mushroom
+// this.moveUp();
+// this.moveRight();
+// this.moveLeft();
+// this.moveUp();
+// this.attackNearbyEnemy();
+
+      //Drink me
+// this.moveRight();
+// this.attackNearbyEnemy();
+// this.moveRight();
+// this.moveDown():
+// this.moveUp();
+// this.moveRight()
+// this.attackNearbyEnemy(); 
+
+    //Taunt the Gaurds
+// this.moveRight();
+// this.say("Hey there!");
+// this.moveLeft();
+// this.moveLeft();
+// this.say("Attack!");
+
+// // Now get Phoebe to follow through the dungeon.
+// this.moveRight();
+// this.say("Follow me.");
+// this.moveRight();
+// this.moveRight();
+// this.moveUp();
+// this.moveDown();
+// this.moveUp();
+// this.moveRight();
+// this.say("Hey There!");
+// this.say("Attack!");
+// this.say("Attack!");
+// this.say("Follow me.");
+// this.moveRight();
+// this.moveRight();
+
+
+      // Its a Trap
+// this.moveDown();
+// this.moveDown();
+// this.say("Over Here!");
+// this.moveUp();
+// this.moveUp();
+// this.say("Attack!");
+
+
+    //Taunt
+// this.say("Hey!");
+// this.say("You smell like an Ogre!");
+// this.say("And you look awful!");
+// this.say("Shreik wasn't even a funny Movie!");
+
+     //Cowardly Taunt 
+//this.moveXY(71,41);
+//this.say("Nah Nah a Boo Boo!");
+//this.moveXY(70, 10);
+
+     //Commanding Followers
+//this.say("Follow Me!");
+//this.moveXY(66,41);
+//this.say("Ogres are DUMB!.. Attack!");
+
+      //Mobile Artillary 
+// this.moveXY(65,29);
+// this.attackXY(68,55);
+// this.attackXY(68,49);
+// this.moveXY(71,52);
+// this.attackXY(50,67);
+// this.attackXY(61,63);
+
  
  
  
