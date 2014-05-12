adf1655b4884cb3de8155318c05182d8f7f9e177
Adrian <modernamun@gmail.com>
2014-05-11 22:53:36 -0400

    Update my_solution.js

--- week_3/4_design_code_combat_mission/my_solution.js
+++ week_3/4_design_code_combat_mission/my_solution.js
@@ -45,16 +45,6 @@ if (anwserTwo === "(35,15)")
 // Refactored Code
 
 
-
-What parts of your strategy worked? What problems did you face?
-What questions did you have while coding? What resources did you find to help you answer them?
-What concepts are you having trouble with, or did you just figure something out? If so, what?
-Did you learn any new skills or tricks?
-How confident are you with each of the Learning Competencies?
-Which parts of the challenge did you enjoy?
-Which parts of the challenge did you find tedious?
-
-
 // Reflection
 //   I was able to get my code to work in the console, but I am not sure if it is what DBC is looking for.   
 //  I was unsure what to do for this challenge so I asked someone from my accountability group for help.  He showed me what he had done in a pairing session with another member of our group.  Thiers was much more witty, and better written.
